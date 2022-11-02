cc54bddb25239a1de467dbf51a01abdd4229f7c4
Tristan Lawrence <trislaw@gmail.com>
2022-11-01 22:52:19 -0400

    clean up unused app code / fix errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ margin: "20px" }}>
+      <p>
+        Run `<strong>npm run storybook</strong>` to have a look at all the
+        available components!
+      </p>
     </div>
   );
 }
